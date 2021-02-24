9b6cdb59a5d82783637b56f3e2142c6792ecaff8
Ben Elliott <benelliott111@gmail.com>
2021-02-24 16:21:18 -0500

    working inquirer

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const Inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
@@ -9,7 +9,148 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const inquirer = require("inquirer");
 
+let team = []
+
+async function getManagerInfo(){
+    let managerInfo = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please enter your manager's name"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter your manager's id"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter your manager's email"
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "Please enter your manager's office number"
+        },
+        {
+            name: "nextstep",
+            type: "list",
+            message: "Please select another team profile to build, or complete the team",
+            choices: ["Build engineer profile", "Build intern profile", "Finish team"]
+        },
+    ])
+
+    team.push(new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber))
+
+    switch (managerInfo.nextstep){
+        case 'Build engineer profile':
+            getEnginnerInfo();
+            break;
+        case 'Build intern profile':
+            getInternInfo();
+            break;
+        case 'Finish team':
+            loadHTML();
+            break; 
+    }
+}
+
+async function getEnginnerInfo(){
+    let enginnerInfo = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please enter your enginner's name"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter your enginner's id"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter your enginner's email"
+        },
+        {
+            name: "gitHub",
+            type: "input",
+            message: "Please enter your enginner's gitHub"
+        },
+        {
+            name: "nextstep",
+            type: "list",
+            message: "Please select another team profile to build, or complete the team",
+            choices: ["Build another engineer profile", "Build intern profile", "Finish team"]
+        },
+    ])
+    team.push(new Engineer(enginnerInfo.name, enginnerInfo.id, enginnerInfo.email, enginnerInfo.gitHub))
+
+    switch (enginnerInfo.nextstep){
+        case 'Build another engineer profile':
+            getEnginnerInfo();
+            break;
+        case 'Build intern profile':
+            getInternInfo();
+            break;
+        case 'Finish team':
+            loadHTML();
+            break; 
+    }
+}
+
+async function getInternInfo(){
+    let interInfo = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please enter your intern's name"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter your intern's id"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter your intern's email"
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "Please enter your intern's school"
+        },
+        {
+            name: "nextstep",
+            type: "list",
+            message: "Please select another team profile to build, or complete the team",
+            choices: ["Build engineer profile", "Build another intern profile", "Finish team"]
+        },
+    ])
+    team.push(new Intern(interInfo.name, interInfo.id, interInfo.email, interInfo.school))
+
+    switch (interInfo.nextstep){
+        case 'Build engineer profile':
+            getEnginnerInfo();
+            break;
+        case 'Build another intern profile':
+            getInternInfo();
+            break;
+        case 'Finish team':
+            loadHTML();
+            break; 
+    }
+    
+}
+
+function loadHTML(){
+    console.log(team)
+}
+getManagerInfo()
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
